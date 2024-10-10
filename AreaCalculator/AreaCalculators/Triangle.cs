24bb9d9f6ec5d5cd0bd6aaeff9fb3e07b84723b0
Vladislav Solokhin <47882145+mrvssq@users.noreply.github.com>
2024-10-10 10:47:40 +0300

    Переименовал _hypotenuse в _side1

--- AreaCalculator/AreaCalculators/Triangle.cs
+++ AreaCalculator/AreaCalculators/Triangle.cs
@@ -2,21 +2,21 @@
 {
     internal class Triangle : BaseFigure
     {
-        private double _hypotenuse;
+        private double _side1;
         private double _side2;
         private double _side3;
         private bool _isRightTriangle;
 
         public Triangle(double side1, double side2, double side3)
         {
-            _hypotenuse = Math.Max(side1, Math.Max(side2, side3));
+            _side1 = Math.Max(side1, Math.Max(side2, side3));
 
-            if (_hypotenuse == side1)
+            if (_side1 == side1)
             {
                 _side2 = side2;
                 _side3 = side3;
             }
-            else if (_hypotenuse == side2)
+            else if (_side1 == side2)
             {
                 _side2 = side1;
                 _side3 = side3;
@@ -35,11 +35,11 @@
 
         override protected bool IsValidFigure()
         {
-            if (_side2 <= 0 || _side3 <= 0 || _hypotenuse <= 0)
+            if (_side2 <= 0 || _side3 <= 0 || _side1 <= 0)
                 return false;
-            return _side2 + _side3 > _hypotenuse &&
-                   _side2 + _hypotenuse > _side3 &&
-                   _side3 + _hypotenuse > _side2;
+            return _side2 + _side3 > _side1 &&
+                   _side2 + _side1 > _side3 &&
+                   _side3 + _side1 > _side2;
         }
 
         override public double Area()
@@ -49,14 +49,14 @@
             }
             else
             {
-                var pHalf = (_hypotenuse + _side2 + _side3) / 2;
-                return Math.Sqrt(pHalf * (pHalf - _hypotenuse) * (pHalf - _side2) * (pHalf - _side3));
+                var pHalf = (_side1 + _side2 + _side3) / 2;
+                return Math.Sqrt(pHalf * (pHalf - _side1) * (pHalf - _side2) * (pHalf - _side3));
             }
         }
 
         private bool IsRightTriangle()
         {
-            return Math.Abs(Math.Pow(_hypotenuse, 2) - (Math.Pow(_side2, 2) + Math.Pow(_side3, 2))) < 1e-10;
+            return Math.Abs(Math.Pow(_side1, 2) - (Math.Pow(_side2, 2) + Math.Pow(_side3, 2))) < 1e-10;
         }
     }
 }

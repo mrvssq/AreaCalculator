4d99a1bb2000b2a468c99fe7cb9953c108abd1ca
Vladislav Solokhin <47882145+mrvssq@users.noreply.github.com>
2024-10-17 15:14:34 +0300

    del Recognizer

--- AreaCalculator/Helpers/AreaCalcHelper.cs
+++ AreaCalculator/Helpers/AreaCalcHelper.cs
@@ -18,7 +18,16 @@ namespace AreaCalculator.Helpers
 
         public static double Recognize(double[] vals)
         {
-            return RecognizeHelper.Recognizer(vals);
+            if (vals.Length == 0)
+                throw new ArgumentException("Не найдено ни одного параметра");
+
+            if (vals.Length == 1)
+                return AreaCalcHelper.Circle(vals[0]);
+
+            if (vals.Length == 3)
+                return AreaCalcHelper.Triangle(vals[0], vals[1], vals[2]);
+
+            throw new ArgumentException("Распознать фигуру по входным данным не удалось");
         }
     }
 }
